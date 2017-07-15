69538680e0983e028722b8ce236ec34ee59fd577
Jack Mastroberti <jmm699@cornell.edu>
2017-07-15 00:49:09 -0400

    added alex simulators

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext.commands import Bot
+import time
+import random
 bot = Bot(command_prefix="!")
+
+
 
 @bot.event
 async def on_ready():
@@ -9,6 +13,21 @@ async def on_ready():
 @bot.command()
 async def hello(*args):
     return await bot.say("Hello, world!")
+
+@bot.command()
+async def sim_alex():
+    await bot.say("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    time.sleep(5)
+    await bot.say("Oh hi grandma, didn't realize you were home...")
+    return None
+
+@bot.command()
+async def sim_alex2():
+    #list of alex's phrases:
+    alex_list = ['Did I tell you about my internship at the zoo?',"I'm getting laid this summer","https://www.youtube.com/watch?v=lQlIhraqL7o","REEEEEEEEEEEEEE"]
+    
+    i = random.randint(1,len(alex_list)) - 1
+    await bot.say(alex_list[i])
 
 # token hidden in a file ignored by git
 bot.run(open('token.txt', 'r').read())
