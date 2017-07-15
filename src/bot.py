4ce0b621e2986090a595b48b3ed736f8bd6119e7
Biraj Dahal <bdahal@g.clemson.edu>
2017-07-15 13:42:04 -0400

    added more to sim command

--- src/bot.py
+++ src/bot.py
@@ -14,27 +14,33 @@ async def hello(*args):
 
 @bot.command()
 async def sim(name):
-	if name.lower() = "alex":
+	if name == None:
+		return await bot.say("Usage: !sim [fname]")
+		
+	if name.lower() == "alex":
 		await bot.say("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 		time.sleep(5)
 		return await bot.say("Oh hi grandma, didn't realize you were home...")
 
+	elif name.lower() == "john":
+		return await bot.say("Does it have anything to do with poles?")
+	
+	elif name.lower() == "biraj":
+		return await bot.say("Yes I'm gay, now give me Delteros")
+		
+	elif name.lower() == "griffin":
+		return await bot.say("Matthew Mercer please have my babies.")
+		
+	elif name.lower() == "conner":
+		return await bot.say("good morning mongoloids")
+		
+	elif name.lower() == "danny":
+		return await bot.say("Yeah I have work thursday, but I can make it work.")
+	
 	else:
 		return await bot.say("Our advanced computers are not able to simulate " + name + " yet.")
-
-@bot.command()
-async def sim_alex():
-    await bot.say("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-    time.sleep(5)
-    await bot.say("Oh hi grandma, didn't realize you were home...")
-    return None
-
     
 #alex_list = ['Did I tell you about my internship at the zoo?',"I'm getting laid this summer","https://www.youtube.com/watch?v=lQlIhraqL7o","REEEEEEEEEEEEEE"]
 
 # token hidden in a file ignored by git
-bot.run(open('token.txt', 'r').read())
-
-#comment2 bitch
-#fuckingchrist
-#watch you damn language
+bot.run(open('./token.txt', 'r').read())
