63903b31911ee2d3effb8ae825fa915ad2dca197
Biraj Dahal <bdahal@g.clemson.edu>
2017-07-27 20:38:04 -0400

    added animation

--- src/bot.py
+++ src/bot.py
@@ -14,8 +14,8 @@ async def hello(*args):
 
 @bot.command(pass_context = True)
 async def auto_react(ctx,ID,string):
-    spaces = ["\\N{BLACK SMALL SQUARE}", "\\N{WHITE SMALL SQUARE}", "\\N{BLACK MEDIUM SQUARE}", "\\N{WHITE MEDIUM SQUARE}", "\\N{BLACK LARGE SQUARE}", "\\N{WHITE LARGE SQUARE}",]
-    await bot.delete_message(ctx.message)
+    #await bot.delete_message(ctx.message)
+    spaces = ["\\N{BLACK SMALL SQUARE}", "\\N{WHITE SMALL SQUARE}", "\\N{BLACK MEDIUM SQUARE}", "\\N{WHITE MEDIUM SQUARE}", "\\N{BLACK LARGE SQUARE}", "\\N{WHITE LARGE SQUARE}"]
     string = string.upper()
     message = await bot.get_message(ctx.message.channel,ID)
     spaceCount = 0
@@ -26,7 +26,66 @@ async def auto_react(ctx,ID,string):
             spaceCount += 1
 
         emoji = emoji.encode().decode('unicode_escape')
-        await bot.add_reaction(message,emoji)
+        await bot.add_reaction(message, emoji)
+
+@bot.command(pass_context = True)
+async def emote_message(ctx,string):
+    #await bot.delete_message(ctx.message)
+    string = string.lower()
+    message = ""
+
+    for char in string:
+        emoji = ''
+        if char.isalpha():
+            emoji = ':regional_indicator_' + char + ': '
+        elif char == ' ':
+            emoji = ':black_large_square: '
+        elif char == '.':
+            emoji = ':white_small_square: '
+        elif char == '?':
+            emoji = ":question: "
+        elif char == '!':
+            emoji = ":exclamation: "
+
+        if len(message + emoji) >= 2000:
+            await bot.say(message)
+            message = emoji
+        else:
+            message += emoji
+
+    await bot.say(message)
+
+@bot.command(pass_context = True)
+async def purge(ctx):
+    #await bot.delete_message(ctx.message)
+    await bot.purge_from(ctx.message.channel, check=lambda msg: (msg.author == bot.user))
+
+@bot.command(pass_context = True)
+async def animate(ctx):
+    #await bot.delete_message(ctx.message)
+    animation = ['(._.)', '( |:)', '(.-.)', '(:| )']
+
+    frame = 0
+    message = await bot.send_message(ctx.message.channel, content=animation[0])
+    while True:
+        time.sleep(2)
+        frame = (frame + 1) % len(animation)
+        await bot.edit_message(message, new_content=animation[frame])
+
+@bot.command(pass_context = True)
+async def flash(ctx, text):
+    #await bot.delete_message(ctx.message)
+    words = text.split()
+
+    frame = 0
+    message = await bot.send_message(ctx.message.channel, content=words[0])
+    await bot.add_reaction(message, "\\N{ALARM CLOCK}".encode().decode('unicode_escape'))
+
+    while True:
+        time.sleep(1)
+        frame = (frame + 1) % len(words)
+        await bot.edit_message(message, new_content=words[frame])
+
 
 @bot.command()
 async def sim(name):
@@ -35,7 +94,7 @@ async def sim(name):
 
 	if name.lower() == "alex":
 		await bot.say("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-		time.sleep(5)
+		asyncio.sleep(5)
 		return await bot.say("Oh hi grandma, didn't realize you were home...")
 
 	elif name.lower() == "john":
